48f369582d2a23befe7ff98d5b10415cb14d2334
maria-natale <m.natale19@studenti.unisa.it>
2019-12-22 11:24:25 +0100

    connection poooling aggiunto

---
+++ FantaFootball/src/DriverManagerConnectionPool.java
@@ -0,0 +1,61 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DriverManagerConnectionPool {
+	private static List<Connection> freeDbConnections;
+
+	static {
+		freeDbConnections = new LinkedList<Connection>();
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("DB driver not found:"+ e.getMessage());
+		} 
+	}
+	
+	private static synchronized Connection createDBConnection() throws SQLException {
+		Connection newConnection = null;
+		String ip = "localhost";
+		String port = "3306";
+		String db = "auth";
+		String username = "root";
+		String password = "root";
+		String url = "jdbc:mysql://localhost:3306/fantacalciounisa?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+		newConnection = DriverManager.getConnection(url, username, password);
+
+		//newConnection.setAutoCommit(false);
+		return newConnection;
+	}
+
+
+	public static synchronized Connection getConnection() throws SQLException {
+		Connection connection;
+
+		if (!freeDbConnections.isEmpty()) {
+			connection = (Connection) freeDbConnections.get(0);
+			freeDbConnections.remove(0);
+
+			try {
+				if (connection.isClosed())
+					connection = getConnection();
+			} catch (SQLException e) {
+				connection.close();
+				connection = getConnection();
+			}
+		} else {
+			connection = createDBConnection();		
+		}
+
+		return connection;
+	}
+
+	public static synchronized void releaseConnection(Connection connection) throws SQLException {
+		if(connection != null) freeDbConnections.add(connection);
+	}
+}
