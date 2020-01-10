f9ee2fcd989977ce1aec6eb4bc1987ecb032b2de
CARAMAo <56078907+CARAMAo@users.noreply.github.com>
2020-01-10 18:10:33 +0100

    fix PostDAO

--- FantaFootball/src/gestoreBacheca/PostDAO.java
+++ FantaFootball/src/gestoreBacheca/PostDAO.java
@@ -1,12 +1,11 @@
 package gestoreBacheca;
 
 import java.sql.Connection;
+
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import db.DriverManagerConnectionPool;
