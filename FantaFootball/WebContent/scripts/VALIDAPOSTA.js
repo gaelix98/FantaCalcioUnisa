656d7692451776dc70d3b9e46e44bcdd98ceeca7
Condor4321 <56090332+Condor4321@users.noreply.github.com>
2020-01-17 06:05:28 +0100

    Add files via upload
    per inserire il post, not sure about it

---
+++ FantaFootball/WebContent/scripts/VALIDAPOSTA.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+	$(".error").hide();
+	$("#form1").submit(function(){
+		var ris = true;
+		$("#form1").find('input').each(function() {
+			if (!validate($(this).attr("name"))){
+				$(this).addClass("er").next().show();
+				ris=false;
+			}
+			else{
+				if($(this).hasClass("er"))
+					$(this).removeClass('er').next().hide();
+			}
+
+		});
+		return ris;
+	});
+
+	function validate(namef){
+		if (namef=="titolo")
+			return validateTitolo();
+		if (namef=="testo")
+			return validateTesto();
+		
+	}
+
+	function validateTitolo(){
+		var nameformat = /^.{5,}$/;
+		if (document.getElementById("titolo").value.match(nameformat)){
+			console.log("corretto");
+			return true;
+		}
+		else{
+			document.getElementById("titolo").focus();
+			console.log("non corretto");
+			return false;
+		}
+	}
+
+	function validateTesto(){
+		var logoformat = /^.{20,}$/;
+		var logobj = document.getElementById("testo");
+		if (logobj.value.match(logoformat))
+			return true;
+		else{
+			document.getElementById("testo").focus();
+			console.log(document.getElementById("testo").value);
+			return false;
+		}
+	}
+
+
+});
